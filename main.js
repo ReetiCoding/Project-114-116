2ef89d1b14886bc6dc57457c6703743242c41698
ReetiCoding <97729209+ReetiCoding@users.noreply.github.com>
2022-01-15 12:35:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+rabbit_x=0;
+rabbit_y=0;
+
 function preload(){
+    rabbit= loadImage("https://i.postimg.cc/T1yQNX29/5c7d78f837a36c40b2dd0e28dff2cc43.png");
 }
 
 function setup(){
@@ -14,10 +18,11 @@ function setup(){
 
 function draw(){
     image(video,0,0,300,300);
+    image(rabbit,rabbit_x,rabbit_y,60,45);
 }
 
 function take_snapshot(){
-    save("my.png");
+    save("my_picture.png");
 }
 
 function modelLoaded(){
@@ -28,6 +33,8 @@ function gotPose(results){
     if(results.length > 0){
         console.log(results);
         console.log("nose x: "+results[0].pose.nose.x);
+        rabbit_x = results[0].pose.nose.x-30;
         console.log("nose y: "+results[0].pose.nose.y);
+        rabbit_y = results[0].pose.nose.y;
     };
 }
